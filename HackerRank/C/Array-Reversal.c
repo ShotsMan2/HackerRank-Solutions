f9ce327f8e5445e8ddb9c93ad9ff1d671efb8e22
ShotsMan2 <uysalselimefe@gmail.com>
2026-02-01 13:35:36 +0300

    Added Array Reversal solution

---
+++ HackerRank/C/Array-Reversal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i, x,last;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    last=num-1;
+
+    for(i=0;i<num/2;i++){
+        x=arr[i];
+        arr[i]=arr[last];
+        arr[last]=x;
+        last--;
+    }
+
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
