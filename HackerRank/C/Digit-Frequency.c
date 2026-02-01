510fc2b0dd9b781ccdc4a6de19d62a9f03897553
ShotsMan2 <uysalselimefe@gmail.com>
2026-02-01 13:38:25 +0300

    Added Digit Frequency solution

---
+++ HackerRank/C/Digit-Frequency.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int array[10]={0},i,len,rakam;
+
+    char str[1000];
+
+    scanf("%s",str);
+
+    len=strlen(str);
+
+    for(i=0;i<len;i++){
+        if(str[i]>='0'&&str[i]<='9'){
+            rakam=str[i]-'0';
+
+            array[rakam]++;
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", array[i]);
+    }
+
+
+    return 0;
+}
