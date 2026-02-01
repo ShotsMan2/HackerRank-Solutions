96ce43721262f5316adf0fd556fc123c64039724
ShotsMan2 <uysalselimefe@gmail.com>
2026-02-01 13:27:27 +0300

    Added Sum of Digits of a Five Digit Number solution

---
+++ HackerRank/C/Sum-of-Digits-of-a-Five-Digit-Number.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n,sum=0,kalan,bolum;
+    scanf("%d", &n);
+
+    sum+=n/10000;
+    kalan=n%10000;
+
+    sum+=kalan/1000;
+    kalan=kalan%1000;
+
+    sum+=kalan/1000;
+    kalan=kalan%1000;
+
+    sum+=kalan/100;
+    kalan=kalan%100;
+
+    sum+=kalan/10;
+    kalan=kalan%10;
+
+    sum+=kalan/1;
+    kalan=kalan%1;
+
+    printf("%d",sum);
+
+
+
+
+
+    return 0;
+}
