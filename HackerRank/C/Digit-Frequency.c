b62aa500292e228641b154c0fc51c17157941c82
ShotsMan2 <uysalselimefe@gmail.com>
2026-02-02 12:10:43 +0300

    Refactor: Applied code formatting

--- HackerRank/C/Digit-Frequency.c
+++ HackerRank/C/Digit-Frequency.c
@@ -3,28 +3,31 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
 
-    int array[10]={0},i,len,rakam;
+    int array[10] = {0}, i, len, rakam;
 
     char str[1000];
 
-    scanf("%s",str);
+    scanf("%s", str);
 
-    len=strlen(str);
+    len = strlen(str);
 
-    for(i=0;i<len;i++){
-        if(str[i]>='0'&&str[i]<='9'){
-            rakam=str[i]-'0';
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            rakam = str[i] - '0';
 
             array[rakam]++;
         }
     }
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         printf("%d ", array[i]);
     }
-
 
     return 0;
 }
