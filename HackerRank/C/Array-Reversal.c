d32ccba0af0cac20fae371bcd8f17982edab42b7
ShotsMan2 <uysalselimefe@gmail.com>
2026-02-02 12:08:12 +0300

    Refactor: Applied code formatting

--- HackerRank/C/Array-Reversal.c
+++ HackerRank/C/Array-Reversal.c
@@ -3,23 +3,25 @@
 
 int main()
 {
-    int num, *arr, i, x,last;
+    int num, *arr, i, x, last;
     scanf("%d", &num);
-    arr = (int*) malloc(num * sizeof(int));
-    for(i = 0; i < num; i++) {
+    arr = (int *)malloc(num * sizeof(int));
+    for (i = 0; i < num; i++)
+    {
         scanf("%d", arr + i);
     }
 
-    last=num-1;
+    last = num - 1;
 
-    for(i=0;i<num/2;i++){
-        x=arr[i];
-        arr[i]=arr[last];
-        arr[last]=x;
+    for (i = 0; i < num / 2; i++)
+    {
+        x = arr[i];
+        arr[i] = arr[last];
+        arr[last] = x;
         last--;
     }
 
-    for(i = 0; i < num; i++)
+    for (i = 0; i < num; i++)
         printf("%d ", *(arr + i));
     return 0;
 }
