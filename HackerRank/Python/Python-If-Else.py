d4fe861ede7f192e6ed33b75e589f21e89d52c33
ShotsMan2 <uysalselimefe@gmail.com>
2026-03-14 22:52:05 +0300

    Added Arithmetic Operators solution

--- HackerRank/Python/Python-If-Else.py
+++ HackerRank/Python/Python-If-Else.py
@@ -1,11 +1,10 @@
-num=int(input())
+num = int(input())
 
-if num%2==1:
+if num % 2 == 1:
     print("Weird")
-elif 2<=num<=5:
+elif 2 <= num <= 5:
     print("Not Weird")
-elif 6<=num<=20:
+elif 6 <= num <= 20:
     print("Weird")
-elif 20<num:
+elif 20 < num:
     print("Not Weird")
-    
